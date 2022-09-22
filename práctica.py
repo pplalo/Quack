43781818d604868f3ffaee42acbd669427933459
José Castaño <jose.eduardo.castagno@gmail.com>
2022-09-21 19:26:11 -0500

    Aproximation of sin(x)

---
+++ práctica.py
@@ -0,0 +1,18 @@
+import numpy as np
+x=np.array([(np.pi/2),(np.pi),(0)])
+term = x
+sum = x
+eps = 1e-8
+n = 2
+
+for i in x:
+    while abs(term/sum)>eps:
+        term = -term*x**2/((2*n-1)*(2*n-2))
+        sum = sum +term
+        n += 1
+        imax = n
+
+resultado = abs(sum-np.sin(x))/np.sin(x)
+
+
+print(x, imax, sum, resultado)
